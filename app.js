aaebc437b5dd5a0815264c3a65378f31554de57d
wendy03 <wendysu03@gmail.com>
2019-12-10 23:17:36 +0800

    add home and user routers

--- app.js
+++ app.js
@@ -14,8 +14,6 @@ const db = require('./models')
 const Todo = db.Todo
 const User = db.User
 
-
-
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -27,31 +25,8 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
+app.use('/users', require('./routes/user'))
 
 // start and listen on the Express server
 app.listen(port, () => {
