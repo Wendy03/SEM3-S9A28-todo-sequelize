e6ab71ffdc4a6ca0434fbe214acb007bd55330f4
wendy03 <wendysu03@gmail.com>
2019-12-12 21:50:47 +0800

    Updat todo model

--- models/todo.js
+++ models/todo.js
@@ -1,7 +1,15 @@
 'use strict';
+const moment = require('moment')
 module.exports = (sequelize, DataTypes) => {
   const Todo = sequelize.define('Todo', {
     name: DataTypes.STRING,
+    note: DataTypes.STRING,
+    dueDate: {
+      type: DataTypes.DATE,
+      get() {
+        return moment(this.getDataValue('dueDate')).format('YYYY-MM-DD')
+      }
+    },
     done: DataTypes.BOOLEAN
   }, {});
   Todo.associate = function (models) {
