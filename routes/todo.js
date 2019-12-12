0d969f8b13789aa237293c2224bec56f1f44c4b9
wendy03 <wendysu03@gmail.com>
2019-12-12 21:46:01 +0800

    Add todo route

---
+++ routes/todo.js
@@ -0,0 +1,90 @@
+const express = require('express')
+const router = express.Router()
+
+// 載入 model
+const db = require('../models')
+const Todo = db.Todo
+const User = db.User
+
+// 載入 auth middleware
+const { authenticated } = require('../config/auth')
+
+// 設定 /todos 路由
+// 列出全部 Todo
+router.get('/', authenticated, (req, res) => {
+  return res.redirect('/', { style: 'index.css' })
+})
+
+// 新增一筆 Todo
+router.post('/', authenticated, (req, res) => {
+  Todo.create({
+    name: req.body.name,
+    note: req.body.note,
+    dueDate: req.body.dueDate,
+    done: false,
+    UserId: req.user.id
+  })
+    .then((todo) => { return res.redirect('/') })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+// 新增一筆  Todo
+router.post('/', authenticated, (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+// 修改 Todo 頁面
+router.get('/:id/edit', authenticated, (req, res) => {
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+      return Todo.findOne({
+        where: {
+          Id: req.params.id,
+          UserId: req.user.id
+        }
+      })
+    })
+    .then((todo) => { return res.render('edit', { todo: todo, style: 'index.css' }) })
+})
+
+// 修改 Todo
+router.put('/:id', authenticated, (req, res) => {
+  Todo.findOne({
+    where: {
+      Id: req.params.id,
+      UserId: req.user.id
+    }
+  })
+    .then((todo) => {
+      todo.name = req.body.name
+      todo.note = req.body.note
+      todo.dueDate = req.body.dueDate
+      todo.done = req.body.done === 'on'
+
+      return todo.save()
+    })
+    .then((todo) => {
+      return res.redirect('/')
+    })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+// 刪除 Todo
+router.delete('/:id', authenticated, (req, res) => {
+  User.findByPk(req.user.id)
+    .then((user) => {
+      if (!user) throw new Error('user not found')
+
+      return Todo.destroy({
+        where: {
+          UserId: req.user.id,
+          Id: req.params.id
+        }
+      })
+    })
+    .then((todo) => { return res.redirect('/') })
+    .catch((error) => { return res.status(422).json(error) })
+})
+
+module.exports = router
