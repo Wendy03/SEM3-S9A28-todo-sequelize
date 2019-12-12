7ba8221ca9ea6c507f0daa5e654ce9a69b145f54
wendy03 <wendysu03@gmail.com>
2019-12-12 21:50:26 +0800

    Add Facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,62 @@
+const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-// 載入 User model
+const FacebookStrategy = require('passport-facebook').Strategy
+const bcrypt = require('bcryptjs')
+
 const db = require('../models')
 const User = db.User
+
 module.exports = passport => {
   passport.use(
-    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+    new LocalStrategy({ usernameField: 'email', passReqToCallback: true }, (req, email, password, done) => {
       User.findOne({ where: { email: email } })
         .then(user => {
           if (!user) {
-            return done(null, false, { message: 'That email is not registered' })
+            return done(null, false, req.flash('warning_msg', '輸入的email未註冊'))
           }
-          if (user.password != password) {
-            console.log('user password not correct.')
-            return done(null, false, { message: 'Email or Password incorrect' })
-          }
-          return done(null, user)
+          bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) throw err
+            if (isMatch) {
+              return done(null, user)
+            } else {
+              return done(null, false, req.flash('warning_msg', 'Email/Password 錯誤!'))
+            }
+          })
         })
+    })
+  )
+
+  passport.use(
+    new FacebookStrategy({
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK,
+      profileFields: ['email', 'displayName']
+    }, (accessToken, refreshToken, profile, done) => {
+      // find and create user
+      User.findOne({
+        where: { email: profile._json.email }
+      }).then(user => {
+        if (!user) {
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt.genSalt(10, (err, salt) =>
+            bcrypt.hash(randomPassword, salt, (err, hash) => {
+              const newUser = new User({
+                name: profile._json.name,
+                email: profile._json.email,
+                password: hash
+              })
+              newUser.save().then(user => {
+                return done(null, user)
+              }).catch(err => {
+                console.log(err)
+              })
+            })
+          )
+        } else {
+          return done(null, user)
+        }
+      })
     })
   )
 
